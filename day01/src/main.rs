6b80035b04f7f036b64292fd92216f2d58cb45bb
James Chuong <chuong.james@gmail.com>
2021-12-05 09:52:12 -0800

    Convert day01 solution to sliding window
    Added arg to solution to allow for adjustable window size

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,19 +1,31 @@
+use std::env;
 use std::io;
 use std::io::BufRead;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let window_size = args[1].to_string().parse::<usize>().unwrap();
     let stdin = io::stdin();
     let handle = stdin.lock();
-    let mut lines = handle.lines();
-    let mut last_input = -1;
+    let lines = handle.lines();
     let mut counter = 0;
+    let mut sum = 0;
 
-    while let Some(line) = lines.next() {
-        let curr = line.unwrap().to_string().parse::<i32>().unwrap();
-        if last_input != -1 && last_input < curr {
+    // parse input into vec before using, and set up our window
+    let numbers = lines.map(|line| line.unwrap().to_string().parse::<u32>().unwrap()).collect::<Vec<u32>>();
+    for n in 0..window_size {
+        sum = sum + numbers[n];
+    }
+    let mut first_number = numbers[0];
+
+    // slide the window by checking new size and removing old number
+    for n in window_size..numbers.len() {
+        let new_sum = sum - first_number + numbers[n];
+        if new_sum > sum {
             counter = counter + 1;
         }
-        last_input = curr;
+        first_number = numbers[n - window_size + 1];
+        sum = new_sum;
     }
     println!("{}", counter);
 }
